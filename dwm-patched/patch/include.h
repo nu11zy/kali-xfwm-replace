3f036313ebfdeb2a1f93517270d37211f1390002
nu11zy <55335158+nu11zy@users.noreply.github.com>
2023-03-27 20:57:54 +0300

    Add files via upload

---
+++ dwm-patched/patch/include.h
@@ -0,0 +1,28 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_awesomebar.h"
+#include "bar_systray.h"
+#include "bar_wintitleactions.h"
+
+/* Other patches */
+#include "attachx.h"
+#include "autostart.h"
+#include "pertag.h"
+#include "placemouse.h"
+#include "push_no_master.h"
+#include "restartsig.h"
+#include "scratchpad.h"
+#include "vanitygaps.h"
+#include "warp.h"
+/* Layouts */
+#include "layout_gapplessgrid.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
