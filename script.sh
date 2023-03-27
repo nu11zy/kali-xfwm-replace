79d4a0d3d418d2d95d4ad3a063ea305b52078991
nu11zy <55335158+nu11zy@users.noreply.github.com>
2023-03-27 20:23:47 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,4 +6,4 @@ git clone --recurse-submodules https://github.com/fairyglade/ly ~/.local/src/ly
 cd ~/.local/src/ly
 make
 sudo make install installsystemd
-sudo systemctl disable lightdm && sudo systemctl enable ly
+sudo systemctl disable lightdm && sudo systemctl enable ly && test -f ./ly.config && sudo cp ly.config /etc/ly/config.ini
