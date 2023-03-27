9e329088a1fe89a0d218c1e31a8b37947deb4f6d
nu11zy <55335158+nu11zy@users.noreply.github.com>
2023-03-27 20:11:41 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,9 @@
 sudo apt update
+sudo apt install build-essential libpam0g-dev libxcb-xkb-dev
 
+# install ly
+git clone --recurse-submodules https://github.com/fairyglade/ly ~/.local/src/ly
+cd ~/.local/src/ly
+make
+sudo make install installsystemd
+sudo systemctl disable lightdm && sudo systemctl enable ly
